91f044d88e37ad00e3866e4bb87b1570a8783b21
theTurtl <addimanni@gmail.com>
2021-12-31 13:05:46 +0100

    updated a small flaw semantic

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -10,18 +10,17 @@ const Profil = () => (
     <Seo title="Profil" />
     <ProfileBanner />
     <div className="profileStat">
-        <h1>Din side</h1>
-        <div className="statContainer">
-          <div className="statCard">
-            <h7>Dine retter</h7>
-            <p>1</p>
-          </div>
-          <div className="statCard">
-            <h7>Dine retter</h7>
-            <p>1</p>
-          </div>
+      <h1>Din side</h1>
+      <div className="statContainer">
+        <div className="statCard">
+          <h7>Dine retter</h7>
+          <p>1</p>
         </div>
-        
+        <div className="statCard">
+          <h7>Dine retter</h7>
+          <p>1</p>
+        </div>
+      </div>
     </div>
     
     <Link to="/">Go back to the homepage</Link>
