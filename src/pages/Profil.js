3c4ab521408a337f14358594abaee39836a1c60a
theTurtl <addimanni@gmail.com>
2022-01-05 13:50:49 +0100

    Updated profile site with correct CSS and HTML.

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -10,16 +10,33 @@ const Profil = () => (
     <Seo title="Profil" />
     <ProfileBanner />
     <div className="profileStat">
-      <h1>Din side</h1>
-      <div className="statContainer">
-        <div className="statCard">
-          <h7>Dine retter</h7>
-          <p>1</p>
+      <div className="yourSide">
+        <h1>Din side</h1>
+        <div className="statContainer">
+          <div className="statCard">
+            <h7>Dine retter</h7>
+            <p>1</p>
+          </div>
+          <div className="statCard">
+            <h7>Dine retter</h7>
+            <p>1</p>
+          </div>
         </div>
-        <div className="statCard">
-          <h7>Dine retter</h7>
-          <p>1</p>
-        </div>
+        <div className="accountDeco"></div>
+      </div>
+      <div className="account">
+        
+        
+          <h2>Konto</h2>
+          <div className="accountBox">
+            <div className="logOut">
+              <p>Logg ut</p>
+            </div>
+            <div className="deleteAccount">
+              <p>Slett konto</p>
+            </div>
+          </div>
+        
       </div>
     </div>
     
