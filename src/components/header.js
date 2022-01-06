3ae58b45f5ded5f65e7245e4722188f774922243
theTurtl <addimanni@gmail.com>
2022-01-06 09:23:18 +0100

    Edited Matforslag page. Bigger banner. Bigger text. Size matters ;)

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+
+
 const Header = ({ siteTitle }) => (
   <header>
     <div class="headerContainer"
